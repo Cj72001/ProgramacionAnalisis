d312e0c61b964ab582c256a4dcb947b38609a96b
OmarLighthouse <oflores@lighthousegroup.dev>
2026-01-06 19:29:55 -0600

    Add: Vars

---
+++ PruebaConsoleApp/IntroSintaxis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// Las llaves {} marcan el comienzo y el final de un bloque de código.
+namespace HelloWorld
+{
+  class Program
+  {
+    //  Todo lo que contenga el metodo "Main" se ejecuta en consola 
+    //  al correr la console app
+    static void Main(string[] args)
+    {
+      // Usando metodo que imprime en consola en la misma linea(Write)
+      Console.Write("Usando metodo que imprime en consola en la misma linea(Write): ");
+      Console.Write("Hello World! ");
+      Console.Write("Hello World! ");
+
+
+      // Usando metodo que imprime en consola (WriteLine) y hace un salto de linea (al final)
+      Console.WriteLine("");
+      Console.WriteLine("-------------------------");
+      Console.WriteLine("Usando metodo que imprime en consola en la siguiente linea(WriteLine): ");
+      Console.WriteLine("Hello World!");
+
+      // Cada instruccion de C# termina con un punto y coma ;
+      // Puedes usar '+' para concatenar textos y variables
+      Console.WriteLine("Hello World!" + " Bye World!");
+      Console.WriteLine("Hello World!");
+
+      // Imprime el resultado de la suma
+      Console.WriteLine("Resultado de suma: " + (1+2) ); 
+
+
+    }
+  }
+}
---
+++ PruebaConsoleApp/Vars/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        // Estructura de declaración de variables
+        // TipoDeDato NombreVariable;
+
+        // Estructura de declaración e inicializacion de variables
+        // TipoDeDato NombreVariable = ValorQueAlmacenaLaVariable;
+
+        // Tipos de variables en C#
+        // Entero (int)
+        int numeroEntero1 = 0;
+        int numeroEntero2 = 1;
+        int numeroEntero3 = 2;
+        int numeroEntero4 = -3;
+      
+        // Decimal (float, double, decimal)
+        float numeroDecimal1 = 0.5f; // (float) se utiliza para representar números con decimales de precisión simple
+        double numeroDecimal2 = 0.0d; // (double) es el tipo de dato decimal por defecto en C#
+        decimal numeroDecimal3 = 0.0m; // (decimal) es utilizado principalmente para valores monetarios
+
+        // Carácter (char)
+        char caracter1 = 'A';
+
+        // Cadena de texto (string)
+        string cadenaTexto1 = "Hola, Mundo!";
+
+        // Booleano (bool)
+        bool booleano1 = true;
+        bool booleano2 = false;
+
+        // Mostrar los valores de las variables en la consola
+        Console.WriteLine("Valor del entero 1: " + numeroEntero1);
+        Console.WriteLine("Valor de la suma de entero 1 y entero 2: " + (numeroEntero1 + numeroEntero2));
+        Console.WriteLine("Valor del float: " + numeroDecimal1);
+        Console.WriteLine("Valor del char: " + caracter1);
+        Console.WriteLine("Valor del string: " + cadenaTexto1);
+        Console.WriteLine("Valor del booleano 1: " + booleano1);
+        Console.WriteLine("Valor del booleano 2: " + booleano2);
+
+        string cadenaTexto2 = "Valor del entero 1:";
+        Console.WriteLine(cadenaTexto2 + numeroEntero1);
+
+    }
+  }
+}
